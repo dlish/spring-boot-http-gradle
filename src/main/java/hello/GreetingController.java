a3c4e42eeb8e7494ca02dca74ccabec3e5b3fd8a
Dustin Lish <dlish27@gmail.com>
2018-11-01 09:17:02 -0500

    update greeting msg

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,12 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello there, %s!";
+    private static final String template = "Hello there from feature-1, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
